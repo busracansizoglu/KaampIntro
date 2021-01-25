7a7f222c9e8417ff474911b26820989a76be52d7
busracansizoglu <busracansizoglu@gmail.com>
2021-01-25 20:20:30 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UrunAdi = "Elma";
+            double Fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun[] urunler = new[] { urun1 }; //array
+            //type-safe--tip-güvenli
+            foreach (Urun urunxx in urunler)
+            {
+                Console.WriteLine(urunxx.Adi);
+                Console.WriteLine(urunxx.Fiyati);
+                Console.WriteLine(urunxx.Aciklama);
+                Console.WriteLine("------------------");
+
+
+            }
+
+            Console.WriteLine("-------------Metotlar------------");
+
+            SepetMenegar sepetMenegar = new SepetMenegar();
+            SepetMenegar.Ekle(urun1);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetMenegar
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler, Sepete Eklendi :" + urun.Adi);
+        }
+    }
+}
+
