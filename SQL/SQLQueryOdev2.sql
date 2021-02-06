26e0d81f89c28478e7366e24999a7d10b92b5c3b
busracansizoglu <busracansizoglu@gmail.com>
2021-02-06 17:44:47 +0300

    Odev 2

---
+++ SQL/SQLQueryOdev2.sql
@@ -0,0 +1,7 @@
+﻿Select Products.ProductName ÜrünAdı, 
+SUM([Order Details].UnitPrice * [Order Details].Quantity)
+[Kazanılan Toplam Miktar] 
+from Products
+Inner Join [Order Details] on [Order Details].ProductID = Products.ProductID 
+Inner Join Orders on Orders.OrderID = [Order Details].OrderID 
+Group By ProductName
